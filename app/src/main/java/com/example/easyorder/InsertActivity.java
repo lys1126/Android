5c605c4c9fb0813e9d60eeebe794d6095338d166
LYS <seon04260@gmail.com>
2021-10-27 17:01:19 +0900

    db 연동을 이용한 spinner 목록 불러오기

--- app/src/main/java/com/example/easyorder/InsertActivity.java
+++ app/src/main/java/com/example/easyorder/InsertActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -34,6 +43,37 @@ public class InsertActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
 
+        String martInfoURL = "http://61.105.122.125/android/martList.php";
+        URLConnector task = new URLConnector(martInfoURL);
+        task.start();
+
+        try{
+            task.join();
+            System.out.println("waiting... for result");
+        }
+        catch(InterruptedException e){
+
+        }
+
+        String result = task.getResult();
+        String[] nmArr = {};
+        try {
+            JSONObject martList = new JSONObject(result);
+            JSONArray ja = martList.getJSONArray("result");
+            nmArr = new String[ja.length()];
+            for(int i=0; i<ja.length(); i++) {
+                JSONObject jo = ja.getJSONObject(i);
+                nmArr[i] = jo.getString("mart_nm");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nmArr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         Button btn_camera = (Button) findViewById(R.id.btn_camera);
         btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +84,6 @@ public class InsertActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
-
 
     }
 }
